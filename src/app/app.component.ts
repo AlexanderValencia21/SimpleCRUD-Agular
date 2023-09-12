3c276ef216fc69d2a4983fdbb59f77a9a5882bb2
AlexanderValencia21 <alexandervalencia0121@gmail.com>
2023-09-12 17:33:24 -0500

    upload project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
-
+import { Employee } from './models/employee';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'angular-simple-crud';
+  employeeArray: Employee[] = [
+    { id: 1, name: 'Alex', country: 'USA' },
+    { id: 2, name: 'Isa', country: 'USA' },
+  ];
+
+  selectedEmployee: Employee = new Employee();
+  openForEdit(employee: Employee) {
+    this.selectedEmployee = employee;
+  }
+  delete() {
+    if (confirm('Are u sure u want to delete it?')) {
+      this.employeeArray = this.employeeArray.filter(
+        (x) => x != this.selectedEmployee
+      );
+      this.selectedEmployee = new Employee();
+    }
+  }
+  addOrEdit() {
+    if (this.selectedEmployee.id === 0) {
+      this.selectedEmployee.id = this.employeeArray.length + 1;
+      this.employeeArray.push(this.selectedEmployee);
+    }
+
+    this.selectedEmployee = new Employee();
+  }
 }
